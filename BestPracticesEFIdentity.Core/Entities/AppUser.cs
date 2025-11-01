56a5ee43abd2eef0aa1d6e9296ef0f710aebc1c3
ali-osman-de <demirkolluosmanali@gmail.com>
2025-11-02 00:47:14 +0300

    Add project files.

---
+++ BestPracticesEFIdentity.Core/Entities/AppUser.cs
@@ -0,0 +1,7 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace BestPracticesEFIdentity.Core.Entities;
+
+public class AppUser : IdentityUser
+{
+}
---
+++ BestPracticesEFIdentity.Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using BestPracticesEFIdentity.Core.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace BestPracticesEFIdentity.Data;
+
+public class AppDbContext : IdentityDbContext<AppUser, AppRole, string>
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+
+
+}
