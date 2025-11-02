c3fe09d65a5290fea5cdce62c9124e7488be4907
ali-osman-de <demirkolluosmanali@gmail.com>
2025-11-02 22:38:09 +0300

    Quick Fix

--- BestPracticesEFIdentity.Web/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace BestPracticesEFIdentity.Web.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
